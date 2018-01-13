c0e1db7211e78c55cd48100853773fc789f50af1
Kim KiBeom <kimki1124@naver.com>
2018-01-14 04:56:08 +0900

    2018.01.14 플러그인 폼 UI

--- src/main/resources/static/resources/js/custom/main.js
+++ src/main/resources/static/resources/js/custom/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     //언어 SELECT OPTION ITEM 세팅
     initLanguageItem();
 
+    //Command Line 플러그인의 data-output 스위치버튼 세팅
+    initDataOutputSwitchBtn();
+
     // 플러그인 mouseover 이벤트
     $('#plugins > li').mouseover(function(){
         // active 클래스가 있는 노드와 없는 노드가 스타일이 다름
@@ -27,6 +30,11 @@ $(document).ready(function(){
 
     // 플러그인 click 이벤트
     $('#plugins > li').click(function(){
+
+        //Toolbar는 개발중이므로 막아놓음
+        if($(this).attr('name') == 'toolbar'){
+            return;
+        }
         onClickPluginDiv($(this), $(this).attr('name'));
     });
 });
@@ -56,6 +64,44 @@ $("#convert").click(function(){
             console.log(request.responseText);
         }
     });
+});
+
+/**
+ * Autoloader 플러그인 내 Language 추가 버튼 클릭 이벤트
+ */
+$('#btn-lang-add').click(function(){
+    var lang = $('#dependencies-language option:selected').text();
+
+    if(isLangAdded(lang)){
+        alert('이미 추가되어있는 언어입니다.')
+        return;
+    }
+    isLangAdded(lang);
+
+    var append = '';
+    append = '<span class="badge badge-info" id="badge-'+lang+'">';
+    append += lang;
+    append += '&nbsp;&nbsp;<i class="fa fa-times pointer" aria-hidden="true" onclick="removeDepLang(\''+lang+'\');"></i>';
+    append += '</span>';
+
+    $('#dep-lang-addon').append(append);
+});
+
+/**
+ * Command Line 플러그인의 data-output 스위치 버튼 클릭이벤트
+ */
+$('#dataOutputYn').on('switchChange.bootstrapSwitch', function(event, state){
+   var isChecked = $('#dataOutputYn')[0].checked;
+
+   if(isChecked){
+       // true
+       $('#dataOutputForm').css('display', 'block');
+       $('#dataOutputLabelForm').css('display', 'block');
+   }else{
+       // false
+       $('#dataOutputForm').css('display', 'none');
+       $('#dataOutputLabelForm').css('display', 'none');
+   }
 });
 
 /**
@@ -95,23 +141,35 @@ function onClickPluginDiv(view, name){
 }
 
 /**
- * Autoloader 플러그인 내 Language 추가 버튼 클릭 이벤트
- */
-$('#btn-lang-add').click(function(){
-    var lang = $('#dependencies-language option:selected').text();
-    var append = '';
-    append = '<span class="badge badge-info" id="badge-'+lang+'">';
-    append += lang;
-    append += '&nbsp;&nbsp;<i class="fa fa-times pointer" aria-hidden="true" onclick="removeDepLang(\''+lang+'\');"></i>';
-    append += '</span>&nbsp;&nbsp;';
-
-    $('#dep-lang-addon').append(append);
-});
-
-/**
  * Autoloader 플러그인 내 Language 뱃지 삭제 버튼 이벤트
  * @param lang
  */
 function removeDepLang(lang){
     $('#badge-'+lang).remove();
+}
+
+/**
+ * Autoloader 플러그인 내 Language 추가 이미 추가되어있는지 체크
+ * @param lang
+ * @returns {boolean}
+ */
+function isLangAdded(lang){
+    var isAdded = false;
+    var addon = $('#dep-lang-addon > span');
+
+    for(var i=0;i<addon.length;i++){
+        var targetLang = $(addon[i]).text().trim();
+        if(lang == targetLang){
+            isAdded = true;
+        }
+    }
+
+    return isAdded;
+}
+
+/**
+ * Command Line 플러그인의 data-output 스위치버튼 세팅
+ */
+function initDataOutputSwitchBtn(){
+    $("[name='dataOutputYn']").bootstrapSwitch();
 }
